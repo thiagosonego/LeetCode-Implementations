0d58001014ddd6c36fd1697c9731478cd747ce1a
thiagosonego <74835122+thiagosonego@users.noreply.github.com>
2024-07-05 15:41:28 -0300

    Time: 88 ms (60.8%), Space: 45 MB (63.04%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cs
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cs
@@ -3,22 +3,35 @@ public class Solution {
         int size = height.Length;
         if (size == 0) return 0;
 
-        int[] l = new int[size];
-        l[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            l[i] = Math.Max(l[i - 1], height[i]);
-        }
-
-        int[] r = new int[size];
-        r[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
-            r[i] = Math.Max(r[i + 1], height[i]);
-        }
-        
+        int highestValue = height[0];
+        int highestIndex = 0;
+        int tempWater = 0;
         int totalWater = 0;
 
-        for(int p = 0; p < size; p ++){
-            totalWater += Math.Min(l[p], r[p]) - height[p];
+        for(int i = 0; i < height.Length; i++) {
+            if(highestValue > height[i]) {
+                tempWater += highestValue - height[i];
+            }
+            else {
+                highestValue = height[i];
+                highestIndex = i;
+                totalWater += tempWater;
+                tempWater = 0;
+            }
+        }
+
+        highestValue = height[height.Length - 1];
+        tempWater = 0;
+
+        for(int i = height.Length -1; i >= highestIndex; i--) {
+            if(highestValue > height[i]) {
+                tempWater += highestValue - height[i];
+            }
+            else {
+                highestValue = height[i];
+                totalWater += tempWater;
+                tempWater = 0;
+            }
         }
 
         return totalWater;
