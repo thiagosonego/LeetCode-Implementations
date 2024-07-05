8fcf07a01ddc91fbb50715abea0d3dca12c9a594
thiagosonego <74835122+thiagosonego@users.noreply.github.com>
2024-07-05 13:38:05 -0300

    Time: 60 ms (52.51%), Space: 44.3 MB (90.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -14,18 +14,26 @@ public class Solution {
     public const int I = 1;
 
     public string IntToRoman(int num) {
+        if (num >= (M*3)) return "MMM" + IntToRoman(num - (M*3));
+        if (num >= (M*2)) return "MM" + IntToRoman(num - (M*2));
         if (num >= M) return "M" + IntToRoman(num - M);
         if (num >= CM) return "CM" + IntToRoman(num - CM);
         if (num >= D) return "D" + IntToRoman(num - D);
         if (num >= CD) return "CD" + IntToRoman(num - CD);
+        if (num >= (C*3)) return "CCC" + IntToRoman(num - (C*3));
+        if (num >= (C*2)) return "CC" + IntToRoman(num - (C*2));
         if (num >= C) return "C" + IntToRoman(num - C);
         if (num >= XC) return "XC" + IntToRoman(num - XC);
         if (num >= L) return "L" + IntToRoman(num - L);
         if (num >= XL) return "XL" + IntToRoman(num - XL);
+        if (num >= (X*3)) return "XXX" + IntToRoman(num - (X*3));
+        if (num >= (X*2)) return "XX" + IntToRoman(num - (X*2));
         if (num >= X) return "X" + IntToRoman(num - X);
         if (num >= IX) return "IX" + IntToRoman(num - IX);
         if (num >= V) return "V" + IntToRoman(num - V);
         if (num >= IV) return "IV" + IntToRoman(num - IV);
+        if (num >= (I*3)) return "III" + IntToRoman(num - (I*3));
+        if (num >= (I*2)) return "II" + IntToRoman(num - (I*2));
         if (num >= I) return "I" + IntToRoman(num - I);
         return "";
     }
